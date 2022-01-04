a97aab1b600e4dc82504b1029640207924722d28
ischanx <ischanx@foxmail.com>
2022-01-04 17:39:15 +0800

    chore: :tada: Midway脚手架搭建项目

---
+++ src/controller/api.ts
@@ -0,0 +1,20 @@
+import { Inject, Controller, Provide, Query, Get } from '@midwayjs/decorator';
+import { Context } from 'egg';
+import { IGetUserResponse } from '../interface';
+import { UserService } from '../service/user';
+
+@Provide()
+@Controller('/api')
+export class APIController {
+  @Inject()
+  ctx: Context;
+
+  @Inject()
+  userService: UserService;
+
+  @Get('/get_user')
+  async getUser(@Query() uid: string): Promise<IGetUserResponse> {
+    const user = await this.userService.getUser({ uid });
+    return { success: true, message: 'OK', data: user };
+  }
+}
