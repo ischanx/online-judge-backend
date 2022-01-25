f4049eb793f5ea6c70ea106b5f36391fee961c6d
ischanx <ischanx@foxmail.com>
2022-01-25 22:06:58 +0800

    feat: 加入测试用的硬编码token

--- src/middleware/tokenVerify.ts
+++ src/middleware/tokenVerify.ts
@@ -12,20 +12,22 @@ export class tokenVerify implements IWebMiddleware {
     return async (ctx: Context, next: IMidwayWebNext) => {
       try {
         await next();
-        if(!ctx.state.user.expireTime || (Date.now() > ctx.state.user.expireTime)){
-          ctx.status = 401;
-          ctx.body = {
-            code: 401,
-            message: 'token过期',
-          };
-        }else{
-          const valid = await this.redisService.exists("token" + ctx.header.authorization.replace("Bearer ",""));
-          if(!valid) {
+        if(ctx.header.authorization !== "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiNjFlYzFiM2IyMGE0YzkzNTY0YTYwZThjIiwiZXhwaXJlVGltZSI6MTY0Mjk0Njc4Njc5NywiaWF0IjoxNjQyOTQ0OTg2fQ.weXxX7OGD3Yv7mXbXa3xIP9O_weJ_CU9ZjHmSUgx5cg"){
+          if(!ctx.state.user.expireTime || (Date.now() > ctx.state.user.expireTime)){
             ctx.status = 401;
             ctx.body = {
               code: 401,
-              message: 'token失效',
+              message: 'token过期',
             };
+          }else{
+            const valid = await this.redisService.exists("token" + ctx.header.authorization.replace("Bearer ",""));
+            if(!valid) {
+              ctx.status = 401;
+              ctx.body = {
+                code: 401,
+                message: 'token失效',
+              };
+            }
           }
         }
       } catch (err) {
